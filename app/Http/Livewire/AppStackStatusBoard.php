c4389b5b6ecc0fb9382308939575353a25f115ba
hinchk <kasey.hinchman@gmail.com>
2020-06-24 20:50:36 -0700

    status widgets

---
+++ app/Http/Livewire/AppStackStatusBoard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AppStackStatusBoard extends Component
+{
+    public function render()
+    {
+        return view('livewire.app-stack-status-board');
+    }
+}
